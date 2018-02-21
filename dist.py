f13a4d94366a0fdc773d3a0f3074aa1099a100fc
Philip Chimento <philip@endlessm.com>
2018-02-20 22:17:31 -0800

    Add Endless logo as brand logo
    SVG created by opening the EPS version of the logo, isolating the mark,
    deleting everything else, and resizing the canvas to the size of the mark.
    
    https://phabricator.endlessm.com/T21126

--- dist.py
+++ dist.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 import os
+import os.path
+import shutil
 import subprocess
 import sys
 
 themedir = sys.argv[1]
-distfilename = sys.argv[2]
+srcdir = sys.argv[2]
+archivename = sys.argv[3]
 
+for extra in ['images', 'templates']:
+    extradir = os.path.join(srcdir, extra)
+    for f in os.listdir(extradir):
+        shutil.copy(os.path.join(extradir, f), os.path.join(themedir, extra))
+
+distfilename = os.path.join(srcdir, archivename)
 cmd = ['tar', '-cJf', distfilename] + os.listdir(themedir)
 
 subprocess.check_call(cmd, cwd=themedir)
